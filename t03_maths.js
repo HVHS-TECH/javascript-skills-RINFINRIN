ce462daaeead5e5895838bc6f9e1eb715794e48e
RINFINRIN <24154rs@hvhs.school.nz>
2026-04-30 01:01:33 +0000

    Apr 30, 2026, 1:01 AM

--- t03_maths.js
+++ t03_maths.js
@@ -8,6 +8,7 @@ console.log("As of 2026 I am 15 years old")
 console.log("I was born in ",   2000 + 10); 
 console.log("in 10 years I will be ",   10 + 15 )("years old"); 
 console.log("I do not feel like telling anybody how much money I have because that is quite confidential infomation.")
+console.log("If I spend half of my money I will have ",   1 + 0);
 
 // This is a single line comment
 

bd12ae4986672f4302a00843a8f7ac8c64aeba3f
RINFINRIN <24154rs@hvhs.school.nz>
2026-04-30 01:25:47 +0000

    Apr 30, 2026, 1:25 AM

--- t04_debugging.js
+++ t04_debugging.js
@@ -1,4 +1,4 @@
-konsole.log["Task 4 - Debugging")
+konsole.log("Task 4 - Debugging")
 
 // calculate the area
 var area = length + width;
@@ -18,5 +18,5 @@ console.log("The same piece of paper has a perimeter of "+perimeter)
 if (perimeter > area){
     console.log("Yay, your area is larger, this is right for this example")
 }else{
-    console.log("Hmm, your perimeter is larger. It shouldn't be for this example)
+    console.log("Hmm, your perimeter is larger. It shouldn't be for this example")
 }

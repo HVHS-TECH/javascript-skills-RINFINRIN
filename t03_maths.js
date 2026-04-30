f691ecb2806ca7115a6a14cc32763d25cd154cfc
RINFINRIN <24154rs@hvhs.school.nz>
2026-04-30 00:57:20 +0000

    Apr 30, 2026, 12:57 AM

--- t03_maths.js
+++ t03_maths.js
@@ -6,6 +6,7 @@ console.log("Running t01_introduction.js")
 console.log("Hello my name is Ryan Szeto.")
 console.log("As of 2026 I am 15 years old")
 console.log("I was born in ",   2000 + 10); 
+console.log("in 10 years I will be ",   10 + 15 )("years old"); 
 console.log("I do not feel like telling anybody how much money I have because that is quite confidential infomation.")
 
 // This is a single line comment

e00bbdf2e4f727e1b7d964017a96a3ee9a328766
RINFINRIN <24154rs@hvhs.school.nz>
2026-05-01 02:46:53 +0000

    May 1, 2026, 2:46 AM

--- t02_variables.js
+++ t02_variables.js
@@ -5,5 +5,11 @@ console.log("Running t02_variables.js")
 
 //variable
 var username = "Ryan";
+var userage = 15;
+var money = "$9000"
+var year = "2026"
 //main code
-console.log("hi "+ My name is Ryan)
+console.log("hi "+ username);
+console.log("I am "+ userage);
+console.log("I have "+ money);
+console.log("The current year is "+ year);

834468073c12fe600bae6c52788743e992b6d502
RINFINRIN <24154rs@hvhs.school.nz>
2026-05-01 02:34:58 +0000

    May 1, 2026, 2:34 AM

--- t02_variables.js
+++ t02_variables.js
@@ -4,7 +4,7 @@ Put your header comment here!
 ****************************/
 console.log("Running t01_introduction.js")
 console.log("Hello my name is Ryan Szeto.")
-console.log("As of 2026 I am 15 years old and my birthday is on the 23rd of October 2026.")
+console.log("As of 2026 I am "15" years old and my birthday is on the 23rd of October 2026.")
 console.log("I do not feel like telling anybody how much money I have because that is quite confidential infomation.")
 
 // This is a single line comment

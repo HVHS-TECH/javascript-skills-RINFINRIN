e8b3c36a815e5e22f90ef975ae9e10a614c2fabc
RINFINRIN <24154rs@hvhs.school.nz>
2026-05-08 02:43:40 +0000

    May 8, 2026, 2:43 AM

--- t05_JavaScript_and_HTML.js
+++ t05_JavaScript_and_HTML.js
@@ -15,5 +15,8 @@ console.log("I was born in "+ (year - 15));
 console.log("I have "+ money);
 console.log("The current year is "+ year);
 
+<div id="JavaScriptOutput">
+    <p>This will be replaced</p>
+</div>
 const OUTPUT = document.getElementById("JavaScriptOutput");
 OUTPUT.innerHTML = "<h2>Added by Javascript</h2>";

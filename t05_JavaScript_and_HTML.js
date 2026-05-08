a6185b03d9c1478d0ee2191d16589b02a940dfa7
RINFINRIN <24154rs@hvhs.school.nz>
2026-05-08 02:57:24 +0000

    May 8, 2026, 2:57 AM

--- t05_JavaScript_and_HTML.js
+++ t05_JavaScript_and_HTML.js
@@ -4,12 +4,12 @@ Task 5
 console.log("Running t05_maths.js")
 
 //variable
-var username = "Ryan";
+var userName = "Ryan";
 var userage = 15;
 var money = "$9000"
 var year = "2026"
 //main code
-console.log("hi "+ username);;
+console.log("hi "+ userName);;
 console.log("As of "+ year + "I am "+ userage);
 console.log("I was born in "+ (year - 15));
 console.log("I have "+ money);
@@ -17,6 +17,6 @@ console.log("The current year is "+ year);
 
 const OUTPUT = document.getElementById("JavaScriptOutput")
 OUTPUT.innerHTML = "<h2>Added by JavaScript</h2>";
-OUTPUT.innerHTML += "<p>Hello " + username + "</p>";
+OUTPUT.innerHTML += "<p>Hello " + userName + "</p>";
 OUTPUT.innerHTML += "<p>Second paragraph line.</p>";
 OUTPUT.innerHTML += "<p>Thrird Paragraph line.</p>"

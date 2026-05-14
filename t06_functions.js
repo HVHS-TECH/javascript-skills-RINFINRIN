754fa5ac96eefdc272b7a4bcbcac0c429c016bb8
RINFINRIN <24154rs@hvhs.school.nz>
2026-05-14 00:55:00 +0000

    May 14, 2026, 12:55 AM

--- t06_functions.js
+++ t06_functions.js
@@ -1,1 +1,22 @@
-/* Paste your code from task 5 here */
+/****************************
+Task 6
+****************************/
+console.log("Running t06_functions.js")
+
+//variable
+var userName = "Ryan";
+var userage = 15;
+var money = "$9000"
+var year = "2026"
+//main code
+console.log("hi "+ userName);;
+console.log("As of "+ year + "I am "+ userage);
+console.log("I was born in "+ (year - 15));
+console.log("I have "+ money);
+console.log("The current year is "+ year);
+
+const OUTPUT = document.getElementById("JavaScriptOutput")
+OUTPUT.innerHTML = "<h2>Added by JavaScript</h2>";
+OUTPUT.innerHTML += "<p>Hello " + userName + "</p>";
+OUTPUT.innerHTML += "<p>I was born in " + year + "</p>";
+OUTPUT.innerHTML += "<p>I don't really know what else to put here</p>"

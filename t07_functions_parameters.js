08c90ad4bcdc03b9b1187b3e0defa05391f8d036
RINFINRIN <24154rs@hvhs.school.nz>
2026-05-14 01:13:02 +0000

    May 14, 2026, 1:13 AM

--- t07_functions_parameters.js
+++ t07_functions_parameters.js
@@ -1,1 +1,11 @@
-/* Paste your code from task 6 here */
+/****************************
+Task 7
+****************************/
+console.log("Running t07_functions_parameters.js")
+
+const OUTPUT = document.getElementById("spaceForJavaScriptOutput")
+function welcome(){
+    OUTPUT.innerHTML += "<p>Welcome to the shop</p>";
+}
+
+welcome()
